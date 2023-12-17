71d7ca1a396c3ae93815eff63819bf4191a13b2e
sdaysegul <sdaysegul@gmail.com>
2023-12-17 15:33:55 +0300

    Initial Commit

---
+++ saveWords/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  saveWords
+//
+//  Created by Ayşegül Gökçe on 17.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
